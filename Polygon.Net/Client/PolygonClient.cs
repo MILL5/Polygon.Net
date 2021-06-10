42152100e4a81d35d611cd739f5f6d7e61497475
mishyjari <mishyjari@outlook.com>
2021-06-10 16:42:30 -0400

    reorder usings

--- Polygon.Net/Client/PolygonClient.cs
+++ Polygon.Net/Client/PolygonClient.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-using AutoMapper;
 using static Pineapple.Common.Preconditions;
 
 namespace Polygon.Net
