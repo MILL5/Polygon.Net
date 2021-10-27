ff4e4a56127a01c41978180a314dca388a3f28a6
Esteban Lalinde <84730201+estebanl5@users.noreply.github.com>
2021-10-27 14:40:37 -0500

    fix property types and added declaredDate (#19)

--- Polygon.Net/Models/StockSplit.cs
+++ Polygon.Net/Models/StockSplit.cs
@@ -11,18 +11,18 @@ namespace Polygon.Net
         public string ExDate { get; set; }
 
         [JsonProperty("forfactor")]
-        public string ForFactor { get; set; }
+        public int ForFactor { get; set; }
 
         [JsonProperty("paymentDate")]
         public string PaymentDate { get; set; }
 
         [JsonProperty("ratio")]
-        public string Ratio { get; set; }
+        public decimal Ratio { get; set; }
 
         [JsonProperty("ticker")]
         public string Ticker { get; set; }
 
         [JsonProperty("tofactor")]
-        public string ToFactor { get; set; }
+        public int ToFactor { get; set; }
     }
 }
